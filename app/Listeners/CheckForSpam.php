1a37902e02f7c6074458e5bbaa5ce21edf3b2145
unknown <harithkhairol@gmail.com>
2018-07-21 01:18:36 +0800

    Feat | Event

---
+++ app/Listeners/CheckForSpam.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ThreadCreated;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class CheckForSpam implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  ThreadCreated  $event
+     * @return void
+     */
+    public function handle(ThreadCreated $event)
+    {
+        //
+        var_dump('Checking for spam');
+
+    }
+}
